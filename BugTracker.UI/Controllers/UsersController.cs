ff0725cff801ba45a627d9783cd47d9b1999397a
Alex Radice <Alex.Radice@bjss.com>
2019-05-21 02:14:50 +0100

    Now users work

---
+++ BugTracker.UI/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BugTracker.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTracker.UI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersController : ControllerBase
+	{
+		private readonly IUserRepository _userRepository;
+
+		public UsersController(IUserRepository userRepository)
+		{
+			_userRepository = userRepository;
+		}
+
+		[HttpGet]
+		public Task<IEnumerable<User>> FetchAll()
+		{
+			return _userRepository.GetUsersAsync();
+		}
+
+		[HttpPost]
+		public Task<User> Create([FromBody] NewUser newUser)
+		{
+			return _userRepository.CreateUserAsync(newUser);
+		}
+
+		[HttpPut]
+		public Task Update([FromBody] User bug)
+		{
+			return _userRepository.UpdateUserAsync(bug);
+		}
+	}
+}
